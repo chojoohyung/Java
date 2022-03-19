4062049e542f0f36810eccddb48491c24d9a25d1
terry <terry0791@naver.com>
2022-03-19 17:44:04 +0900

    commit

---
+++ src/javaa/printEX01.java
@@ -0,0 +1,13 @@
+package javaa;
+
+public class printEX01 {
+
+	public static void main(String[] args) {
+		int cash = 2000;
+		int snack = 200;
+		int change = cash - snack;
+		System.out.println("잔돈 = "+change+"");
+
+	}
+
+}
