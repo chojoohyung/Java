c1ec65e320e8731712f0c15e9b98d2a7654f0271
terry <terry0791@naver.com>
2022-03-19 17:25:19 +0900

    commit

--- src/javaa/jav.java
+++ src/javaa/jav.java
@@ -3,7 +3,7 @@ package javaa;
 public class jav {
 
 	public static void main(String[] args) {
-		System.out.println("I LOVE YUME");
+		System.out.println("I LOVE YUme");
 
 	}
 
--- src/javaa/jav2.java
+++ src/javaa/jav2.java
@@ -3,7 +3,7 @@ package javaa;
 public class jav2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("I LOVE YU");
 
 	}
 
