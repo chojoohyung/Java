4f701453770029f6944505f356d26b86ead51df8
terry <terry0791@naver.com>
2022-03-20 18:57:06 +0900

    commit

---
+++ src/javaa/practice.java
@@ -0,0 +1,56 @@
+package javaa;
+
+import java.util.Scanner;
+
+public class practice {
+	public static void main(String[] args) {
+
+/*잔돈
+	int cash =2000;
+	int b = 200;
+	int result = (cash - b);
+	System.out.println("잔돈 = "+result+"입니다");
+
+	
+
+*/
+		
+	// 과목 평균
+		/*
+		
+	int kor = 80;
+	int math = 90;
+	int eng = 100;
+	int avg = (kor + math + eng) / 3; 
+	System.out.println("평균은 :"+ avg + "입니다."); */
+		
+	// 
+		
+	/*Scanner sc = new Scanner(System.in);
+	
+	System.out.println("이름을 입력하세요");
+	String name = sc.next();
+	
+	System.out.println("자바 점수를 입력하세요");
+	int java = sc.nextInt();
+	
+	System.out.println("리눅스 점수를 입력하세요");
+	int linux = sc.nextInt();
+	
+	System.out.println(name + "님의 과목 합계는 =" + (java + linux) + "점" + "\n"+ "평균은=" + (java + linux) / 2 + "입니다");
+	
+	if(java>linux) {
+		System.out.println("리눅스 공부 더 하세요");
+	}
+	
+	else if(java<linux) {
+		System.out.println("리눅스 공부 더 하세요");
+	}
+	else {
+		System.out.println("둘 다 공부 더하세요");
+	}*/
+	
+		}
+}
+
+		
